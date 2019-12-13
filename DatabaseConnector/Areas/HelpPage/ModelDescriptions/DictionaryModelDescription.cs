fc0bd71f997aaac5637959a3c772cffa2398cc02
Brandon <43607839+brann0n@users.noreply.github.com>
2019-12-13 16:25:50 +0100

    Added project files

---
+++ DatabaseConnector/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DatabaseConnector.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DatabaseConnector/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DatabaseConnector.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
